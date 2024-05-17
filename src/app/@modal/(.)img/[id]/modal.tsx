a3b7a272218d5dd13fda4eb3a2d2c376150cae68
Karthik Ayangar <karthik.ayangar7118@gmail.com>
2024-05-17 11:19:36 +0530

    dialog and image box styling
    Signed-off-by: Karthik Ayangar <karthik.ayangar7118@gmail.com>

--- src/app/@modal/(.)img/[id]/modal.tsx
+++ src/app/@modal/(.)img/[id]/modal.tsx
@@ -19,9 +19,8 @@ export function Modal({ children }: { children: React.ReactNode }) {
   }
 
   return createPortal(
-      <dialog ref={dialogRef} className="m-0 w-screen h-screen bg-zinc-900/50 flex flex-col justify-center items-center" onClose={onDismiss}>
+      <dialog ref={dialogRef} className="m-0 w-screen h-screen bg-zinc-900/50 p-0 backdrop-blur-md text-white" onClose={onDismiss}>
         {children}
-        <button onClick={onDismiss} className="close-button"/>
       </dialog>,
     document.getElementById('modal-root')!
   );
--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -4,7 +4,14 @@ import { getImage } from "~/server/queries"
 const FullPageImageView = async (props: {id:number}) => {
   const image = await getImage(props.id)
   return (
-      <img src={image.url} alt="" className="w-96"/>
+      <div className="flex w-full h-full min-w-0">
+        <div className="flex-shrink flex justify-center w-full items-center">
+        <img src={image.url} className="max-h-screen flex-shrink object-cover"/>
+        </div>
+        <div className="flex flex-col w-56 flex-shrink-0">
+            <div className="text-xl font-bold ">{image.name}</div>
+        </div>
+      </div>
   )
 }
 
