4ccd55dfa69783ef1e36ff64d2908e34c51317e5
Karthik Ayangar <karthik.ayangar7118@gmail.com>
2024-05-21 20:57:06 +0530

    Custom upload button

---
+++ src/app/_components/simple-upload-button.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useUploadThing } from "~/utils/uploadthing";
+
+// inferred input off useUploadThing
+type Input = Parameters<typeof useUploadThing>;
+
+const useUploadThingInputProps = (...args: Input) => {
+  const $ut = useUploadThing(...args);
+
+  const onChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) return;
+
+    const selectedFiles = Array.from(e.target.files);
+    const result = await $ut.startUpload(selectedFiles);
+
+    console.log("uploaded files", result);
+    // TODO: persist result in state maybe?
+  };
+
+  return {
+    inputProps: {
+      onChange,
+      multiple: ($ut.permittedFileInfo?.config?.image?.maxFileCount ?? 1) > 1,
+      accept: "image/*",
+    },
+    isUploading: $ut.isUploading,
+  };
+};
+
+function UploadSVG() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M9 8.25H7.5a2.25 2.25 0 0 0-2.25 2.25v9a2.25 2.25 0 0 0 2.25 2.25h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25H15m0-3-3-3m0 0-3 3m3-3V15" />
+</svg>
+
+    )
+}
+
+export function SimpleUploadButton() {
+    const router = useRouter()
+    const {inputProps} = useUploadThingInputProps("imageUploader", {
+        onClientUploadComplete(res) {
+            router.refresh();
+        },
+    })
+
+  return (
+    <div>
+        <label htmlFor="upload-button" className="cursor-pointer"><UploadSVG /></label>
+        <input id="upload-button" type="file" className="sr-only" {...inputProps}/> 
+    </div>
+  );
+}
--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,21 +1,17 @@
-"use client"
+
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
-import { useRouter } from "next/navigation"
-import { UploadButton } from "~/utils/uploadthing"
+import { SimpleUploadButton } from "./simple-upload-button"
 
 export const TopNav = () => {
-  const router = useRouter()
     return (
       <nav className="flex items-center justify-between gap-4 p-4 text-2xl border-b-2">
         <div>Gallery</div>
-        <div className="flex flex-row gap-4">
+        <div className="flex flex-row gap-4 items-center">
             <SignedOut>
                 <SignInButton />
             </SignedOut>
             <SignedIn>
-              <UploadButton endpoint="imageUploader" onClientUploadComplete={()=>{
-                router.refresh(); // optimised refresh, provided my next
-              }}/>
+              <SimpleUploadButton />
                 <UserButton/>
             </SignedIn>
         </div>
