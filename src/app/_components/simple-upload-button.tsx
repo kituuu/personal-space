f6c190b2e1b7250ffb00d96efc216022960b03da
Karthik Ayangar <karthik.ayangar7118@gmail.com>
2024-05-21 21:34:05 +0530

    Adds spinner in toast

--- src/app/_components/simple-upload-button.tsx
+++ src/app/_components/simple-upload-button.tsx
@@ -31,34 +31,92 @@ const useUploadThingInputProps = (...args: Input) => {
 };
 
 function UploadSVG() {
-    return (
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M9 8.25H7.5a2.25 2.25 0 0 0-2.25 2.25v9a2.25 2.25 0 0 0 2.25 2.25h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25H15m0-3-3-3m0 0-3 3m3-3V15" />
-</svg>
-
-    )
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="h-6 w-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M9 8.25H7.5a2.25 2.25 0 0 0-2.25 2.25v9a2.25 2.25 0 0 0 2.25 2.25h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25H15m0-3-3-3m0 0-3 3m3-3V15"
+      />
+    </svg>
+  );
+}
+function LoadingSpinnerSVG() {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="white"
+    >
+      <rect
+        className="spinner_9y7u"
+        x="1"
+        y="1"
+        rx="1"
+        width="10"
+        height="10"
+      />
+      <rect
+        className="spinner_9y7u spinner_DF2s"
+        x="1"
+        y="1"
+        rx="1"
+        width="10"
+        height="10"
+      />
+      <rect
+        className="spinner_9y7u spinner_q27e"
+        x="1"
+        y="1"
+        rx="1"
+        width="10"
+        height="10"
+      />
+    </svg>
+  );
 }
 
 export function SimpleUploadButton() {
-    const router = useRouter()
-    const {inputProps} = useUploadThingInputProps("imageUploader", {
-      onUploadBegin() {
-        toast("Uploading...", {
+  const router = useRouter();
+  const { inputProps } = useUploadThingInputProps("imageUploader", {
+    onUploadBegin() {
+      toast(
+        <div className="flex flex-row items-center gap-2 text-lg">
+          <LoadingSpinnerSVG /> Uploading ...{" "}
+        </div>,
+        {
           duration: 100000,
-          id:"upload-begin",
-        })
-      },
-        onClientUploadComplete() {
-          toast.dismiss("upload-begin");
-          toast("Upload Complete!")
-            router.refresh();
+          id: "upload-begin",
         },
-    })
+      );
+    },
+    onClientUploadComplete() {
+      toast.dismiss("upload-begin");
+      toast("Upload Complete!");
+      router.refresh();
+    },
+  });
 
   return (
     <div>
-        <label htmlFor="upload-button" className="cursor-pointer"><UploadSVG /></label>
-        <input id="upload-button" type="file" className="sr-only" {...inputProps}/> 
+      <label htmlFor="upload-button" className="cursor-pointer">
+        <UploadSVG />
+      </label>
+      <input
+        id="upload-button"
+        type="file"
+        className="sr-only"
+        {...inputProps}
+      />
     </div>
   );
 }
