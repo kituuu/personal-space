4b6d7f7f82b21836130ca9638493d169a8c511a2
Karthik Ayangar <karthik.ayangar7118@gmail.com>
2024-06-07 23:15:24 +0530

    feat: SEO things
    Signed-off-by: Karthik Ayangar <karthik.ayangar7118@gmail.com>

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,4 +1,4 @@
-import { auth } from "@clerk/nextjs/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
 import { db } from "~/server/db";
@@ -12,6 +12,10 @@ export const ourFileRouter = {
     .middleware(async ({ req }) => {
       const user = auth();
       if (!user.userId) throw new UploadThingError("Unauthorized");
+
+      const fullUserData = await clerkClient.users.getUser(user.userId);
+      if (fullUserData.privateMetadata["allow-upload"] !== true)
+        throw new UploadThingError("Not allowed to upload");
 
       const { success } = await ratelimit.limit(user.userId);
       if (!success) throw new UploadThingError("Rate limited");
---
+++ src/const.ts
@@ -0,0 +1,13 @@
+export const METADATA = {
+  title: "Personal Space",
+  description:
+    "Your personal sanctuary for cherished memories. Safely store and organize your precious moments with ease.",
+  keywords: "karthik, ayangar, wonder wall, kituuu, gallery",
+  author: "Karthik Ayangar",
+  manifest: `/manifest.json`,
+  web_manifest: `/site.webmanifest`,
+  favicon_16x16: `/favicon-16x16.png`,
+  favicon_32x32: `/favicon-32x32.png`,
+  icon: `/favicon.ico`,
+  apple_touch_icon: `/apple-touch-icon.png`,
+};
